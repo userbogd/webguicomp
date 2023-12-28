bb61573661b3bfcef23478349bbc9aaf5b28ff6f
bogdan <bogd@live.ru>
2023-12-28 22:06:09 +0200

    added AP disable delayed setting

--- cards/WifiSetCard.vue
+++ cards/WifiSetCard.vue
@@ -9,8 +9,12 @@
         <div class="q-gutter-md q-pa-none q-pb-none">
           <q-toggle v-model="data.wifi_enab" label="WiFi enable" />
           <q-btn class="btn" flat label="Scan WiFi" @click="OpenScanDialog()"></q-btn>
-          <q-select v-model="wifimodestr" :options="wifimodes" :map-options="true" :emit-value="true" label="WiFi mode"></q-select>
-          <q-input type="number" v-model="wifipwr" label="Max power, dBm" />
+          <q-select v-model="wifimodestr" :options="wifimodes" :map-options="true" :emit-value="true"
+            label="WiFi mode"></q-select>
+          <q-select v-model="apshutdown" :options="apdshuttime" :map-options="true" :emit-value="true"
+            label="Disable AP after"></q-select>
+          <q-input type="number" v-model="wifipwr"
+            :rules="[val => val <= 20 && val > 0 || 'WiFi otput power range 0...20 dBm']" label="Max power, dBm" />
           <q-input v-model="data.wifi_ap_ssid" label="WiFi network name(AP):" />
           <q-input type="password" v-model="data.wifi_ap_key" label="WiFi network key(AP):" />
           <q-input v-model="data.wifi_ap_ip" label="IP address(AP):" />
@@ -45,7 +49,7 @@ defineOptions({
 })
 
 const init = {
-  wifi_enab: true, wifi_mode: 1, wifi_power: 0, wifi_ap_ssid: "", wifi_ap_key: "",
+  wifi_enab: true, wifi_mode: 1, wifi_power: 0, wifi_disab_time: 0, wifi_ap_ssid: "", wifi_ap_key: "",
   wifi_ap_ip: "", wifi_sta_ssid: "", wifi_sta_key: "", wifi_isdhcp: true, wifi_sta_ip: "",
   wifi_sta_mask: "", wifi_sta_gw: "", wifi_dns1: "", wifi_dns2: "", wifi_dns3: "",
   wifi_sta_mac: "", wifi_ap_mac: ""
@@ -55,6 +59,13 @@ const wifimodes = [
   { label: 'Access point (AP)', value: '2' },
   { label: 'Mixed mode (AP+STA)', value: '3' }];
 
+const apdshuttime = [
+  { label: '1 minute', value: '1' },
+  { label: '5 minutes', value: '5' },
+  { label: '10 minutes', value: '10' },
+  { label: '30 minutes', value: '30' },
+  { label: '60 minutes', value: '60' },
+  { label: 'Never', value: '0' }];
 const data = reactive(init);
 
 
@@ -65,6 +76,11 @@ const wifimodestr = computed({
 const wifipwr = computed({
   get() { return (data.wifi_power) / 4 },
   set(val) { data.wifi_power = val * 4; }
+})
+
+const apshutdown = computed({
+  get() { return (data.wifi_disab_time).toString() },
+  set(val) { data.wifi_disab_time = Number(val); }
 })
 
 function OpenScanDialog() {
